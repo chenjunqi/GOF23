768f5d4e4ed53164b1dce49675c01d73b1ade2b2
junqi.chen <chenjunqi.china@foxmail.com>
2017-02-14 14:43:03 +0800

    add simple factory pattern

--- app/src/main/java/com/bookbuf/gof23/adapter/CommandAdapter.java
+++ app/src/main/java/com/bookbuf/gof23/adapter/CommandAdapter.java
@@ -3,16 +3,8 @@ package com.bookbuf.gof23.adapter;
 import com.bookbuf.gof23.Machine;
 import com.bookbuf.gof23.User;
 import com.bookbuf.gof23.command.ICommand;
-import com.bookbuf.gof23.command.impls.BoilWaterCommandImpl;
 import com.bookbuf.gof23.command.impls.Command;
-import com.bookbuf.gof23.command.impls.CookCommandImpl;
-import com.bookbuf.gof23.command.impls.DanceCommandImpl;
-import com.bookbuf.gof23.command.impls.OpenDoorCommandImpl;
-import com.bookbuf.gof23.command.impls.SongCommandImpl;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
+import com.bookbuf.gof23.simple_factory.CommandSimpleFactory;
 
 // 适配类：用于适配ICommand接口
 public class CommandAdapter implements ICommand {
@@ -29,59 +21,10 @@ public class CommandAdapter implements ICommand {
         this.machine = machine;
     }
 
-    private HashMap<String, Command> map = new HashMap<>();
-
-    // 不同method适配为对应的操作指令
-    private Command pickCommand(String method, String param) {
-        Command command = null;
-        if (method.startsWith(DanceCommandImpl.KEY_DANCE)) {
-            command = createCommand(DanceCommandImpl.KEY_DANCE, param, DanceCommandImpl.class);
-        } else if (method.startsWith(SongCommandImpl.KEY_SONG)) {
-            command = createCommand(SongCommandImpl.KEY_SONG, param, SongCommandImpl.class);
-        } else if (method.startsWith(BoilWaterCommandImpl.KEY_BOIL_WATER)) {
-            command = createCommand(BoilWaterCommandImpl.KEY_BOIL_WATER, param, BoilWaterCommandImpl.class);
-        } else if (method.startsWith(CookCommandImpl.KEY_COOK)) {
-            command = createCommand(CookCommandImpl.KEY_COOK, param, CookCommandImpl.class);
-        } else if (method.startsWith(OpenDoorCommandImpl.KEY_OPEN_DOOR)) {
-            command = createCommand(OpenDoorCommandImpl.KEY_OPEN_DOOR, param, OpenDoorCommandImpl.class);
-            OpenDoorCommandImpl openDoorCommand = (OpenDoorCommandImpl) command;
-            openDoorCommand.setUser(user);
-            openDoorCommand.setMachine(machine);
-        }
-        return command;
-    }
-
-    // 创建过程由内部控制
-    private Command createCommand(String key, String param, Class<?> clazz) {
-        if (map.containsKey(key)) {
-            return map.get(key);
-        }
-        Command command = null;
-        try {
-            Constructor<?> constructor = clazz.getConstructor(String.class);
-            command = (Command) constructor.newInstance(param);
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-        if (command != null) {
-            map.put(key, command);
-            return command;
-        } else {
-            throw new IllegalArgumentException("NO COMMAND CREATED!!!");
-        }
-    }
-
-
     @Override
     public void excute() {
         System.out.println("调用指令：" + toString());
-        Command command = pickCommand(this.method, this.param);
+        Command command = (Command) CommandSimpleFactory.createCommand(this.user, this.machine, this.method, this.param);
         command.excute();
     }
 
@@ -90,7 +33,6 @@ public class CommandAdapter implements ICommand {
         return "CommandAdapter{" +
                 "method='" + method + '\'' +
                 ", param='" + param + '\'' +
-                ", map=" + map +
                 '}';
     }
 }
