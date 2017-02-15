a8a50649b208fa665e609abdb1effd4b5af0b753
junqi.chen <chenjunqi.china@foxmail.com>
2017-02-15 09:57:07 +0800

    add simple factory pattern

--- app/src/main/java/com/bookbuf/gof23/simple_factory/CommandSimpleFactory.java
+++ app/src/main/java/com/bookbuf/gof23/simple_factory/CommandSimpleFactory.java
@@ -17,17 +17,21 @@ import com.bookbuf.gof23.command.impls.SongCommandImpl;
 
 public class CommandSimpleFactory {
 
+    private static boolean equals(String method, String key) {
+        return method.startsWith(key);
+    }
+
     public static ICommand createCommand(User user, Machine machine, String method, String param) {
         Command command;
-        if (method.startsWith(DanceCommandImpl.KEY_DANCE)) {
+        if (equals(method, DanceCommandImpl.KEY_DANCE)) {
             command = new DanceCommandImpl(param);
-        } else if (method.startsWith(SongCommandImpl.KEY_SONG)) {
+        } else if (equals(method, SongCommandImpl.KEY_SONG)) {
             command = new SongCommandImpl(param);
-        } else if (method.startsWith(BoilWaterCommandImpl.KEY_BOIL_WATER)) {
+        } else if (equals(method, BoilWaterCommandImpl.KEY_BOIL_WATER)) {
             command = new BoilWaterCommandImpl(param);
-        } else if (method.startsWith(CookCommandImpl.KEY_COOK)) {
+        } else if (equals(method, CookCommandImpl.KEY_COOK)) {
             command = new CookCommandImpl(param);
-        } else if (method.startsWith(OpenDoorCommandImpl.KEY_OPEN_DOOR)) {
+        } else if (equals(method, OpenDoorCommandImpl.KEY_OPEN_DOOR)) {
             command = new OpenDoorCommandImpl(param, user, machine);
         } else {
             throw new IllegalArgumentException("NO COMMAND CREATED!!!");
