d5bebcce78672593b8b99e5987e9b2c2322f877b
junqi.chen <chenjunqi.china@foxmail.com>
2017-02-17 15:53:24 +0800

    modify prototype chain pattern.

--- app/src/main/java/com/bookbuf/gof23/prototype/IPrototype.java
+++ app/src/main/java/com/bookbuf/gof23/prototype/IPrototype.java
@@ -12,14 +12,13 @@ import java.lang.annotation.Target;
 
 public interface IPrototype {
 
+    // 原型对象（即它的父类对象）
     IPrototype __proto__();
 
+    // 自身构造器
     IPrototype __constructor__();
 
-
-    /**
-     * 定义ProtoTypeInherits方法的目的是为了演示，通过 原型链的"对象继承"
-     */
+    // 定义ProtoTypeInherits方法的目的是为了演示，通过 原型链的"对象继承"
     @Target({ElementType.METHOD})
     @Retention(RetentionPolicy.RUNTIME)
     @interface ProtoTypeInherits {
